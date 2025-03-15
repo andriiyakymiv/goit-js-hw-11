c247530bee2b861cb4680e00a960b2187cf039c3
andrii <a.iakumiv@gmail.com>
2025-03-15 20:38:20 +0200

    Added a check for an empty string without spaces

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,7 +4,12 @@ const refs = {
 
 const onSearchFormSubmit = event => {
   event.preventDefault();
-  const query = event.currentTarget.elements.search_text.value;
+  const query = event.currentTarget.elements.search_text.value.trim();
+
+  if (query === '') {
+    alert('Enter text in the search field');
+    return;
+  }
 };
 
 refs.searchForm.addEventListener('submit', onSearchFormSubmit);
