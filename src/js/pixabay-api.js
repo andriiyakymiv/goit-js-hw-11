87629fb25de5ead91f424d130ba354760f4136c7
andrii <a.iakumiv@gmail.com>
2025-03-15 21:05:56 +0200

    Added a request to the server for images and its

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -10,6 +10,21 @@ const onSearchFormSubmit = event => {
     alert('Enter text in the search field');
     return;
   }
+  fetch(
+    `https://pixabay.com/api/?key=49359014-7397220ae93b07f148b4c236e&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 refs.searchForm.addEventListener('submit', onSearchFormSubmit);
